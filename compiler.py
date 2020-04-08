a03b0e0ac1a9ba114d75462a415c5f041be8ec23
Samuel Knapp <samuel.j.knapp@btinternet.com>
2020-04-08 23:49:06 +0100

    started project to build a custom compiler

--- compiler.py
+++ compiler.py
@@ -1,9 +1,37 @@
 from parser import *
 
-#parser
 source = open("print.k", "r")
 data = source.readlines()
 
+print(data)
+
+#remove file formating a turn into a single string
+file=""
+for line in data:
+    line.strip()
+    for char in line:
+        if(char=='\t'): pass
+        else: file+=char
+
+print(file)
+
 print("starting compile")
 
-parse(data)
+#parser
+parse(file)
+
+t=""
+for line in source:
+    line.strip()
+    for char in line:
+        t+=char
+
+print(t)
+
+
+
+
+assemble = open("ass.asm", "w")
+
+
+
--- parser.py
+++ parser.py
@@ -3,32 +3,44 @@ def parse(data):
     lcount=0    
 
     for line in data:
-        count =0
-        
-        
-        open=0
-        closed=0
-        lenght = len(line)
-        if(line[-1]=='\n'):
-            lenght-=1
+        count=0
+        curly=0
+        startfound=0
 
-        for char in line:
-            count+=1
-            
-            if(char=='('): open+=1
-            elif(char==')'): closed+=1
+        line=line.rstrip()
+        if(line!=""):
+            if(line=="int start(){"):
+                startfound =1
+        
+            open=0
+            lenght = len(line)
+            if(line[-1]=='\n'):
+                lenght-=1
 
-            if(char==';'):
+            for char in line:
+                count+=1
             
-                if(open>closed):
-                    print(f'too many open brackets on line{lcount} with open {open} and closed {closed}')
-                    quit()
-                elif(open<closed):
-                    print(f'too many closed brackets on line{lcount} with open {open} and closed {closed}')
-                    quit()
-            elif(lenght==count and char !=';'): 
-                print(f'missing ; on line {lcount}')
+                if(char=='('): open+=1
+                elif(char==')'): open-=1
+                elif(char=='{'): curly+=1
+                elif(char=='}'): curly-=1
+
+                if(char==';'):
+            
+                    if(open>0):
+                        print(f'too many open brackets on line{lcount} with open {open} and closed {closed}')
+                        quit()
+                    elif(open<0):
+                        print(f'too many closed brackets on line{lcount} with open {open} and closed {closed}')
+                        quit()
+                elif(lenght==count and char !=';'): 
+                    print(f'missing ; on line {lcount}')
         
         lcount+=1
+
+    if(startfound==0):
+        print("main function not found")
+        quit
+
 
 
