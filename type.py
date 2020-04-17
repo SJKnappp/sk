d8c85d2bc91c310d6b32fc3b5523ed035ab075b7
Samuel Knapp <samuel.j.knapp@btinternet.com>
2020-04-17 21:27:11 +0100

    fixed that str was missing causing runtime errors

--- type.py
+++ type.py
@@ -4,31 +4,31 @@ import config as con
 def type(string):
 	name=""
 	if(string=="char"):
-		name="db"+len(con.db)
+		name="db"+str(len(con.db))
 		con.db.append(f'{name}')
 	elif(string=="i8"):
 		name="db"+str(len(con.db))
 		con.db.append(f'{name}')
 	elif(string=="u8"):
-		name="db"+len(con.db)
+		name="db"+str(len(con.db))
 		con.db.append(f'{name}')
 	elif(string=="i16"):
-		name="do"+len(con.do)
+		name="do"+str(len(con.do))
 		con.db.append(f'{name}')
 	elif(string=="u16"):
-		name="do"+len(con.do)
+		name="do"+str(len(con.do))
 		con.db.append(f'{name}')
 	elif(string=="i32"):
-		name="dy"+len(con.dy)
+		name="dy"+str(len(con.dy))
 		con.db.append(f'{name}')
 	elif(string=="u32"):
-		name="dy"+len(con.dy)
+		name="dy"+str(len(con.dy))
 		con.db.append(f'{name}')
 	elif(string=="i64"):
-		name="dz"+len(con.dz)
+		name="dz"+str(len(con.dz))
 		con.db.append(f'{name}')
 	elif(string=="u64"):
-		name="dz"+len(con.dz)
+		name="dz"+str(len(con.dz))
 		con.db.append(f'{name}')
 	elif (string=="void"):
 		name="void"
