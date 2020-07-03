2714b2f057c4c806cc032049f488f32c5111ad16
Samuel Knapp <samuel.j.knapp@btinternet.com>
2020-07-03 05:03:05 +0100

    got a file inport methord which can include user files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "main.h"
 
 // takes and input from file and can be called recursivly
+// error return value 1 too many compiler intrustion input
+// 2 can't open file
 std::string fileInput(std::string text, std::string file) {
 
   std::string line;
@@ -11,29 +13,57 @@ std::string fileInput(std::string text, std::string file) {
   if (src.is_open()) {
     while (getline(src, line)) {
       // checks if theres an include statment
-      bool ignore = false;
+      // bool ignore = false;
       std::string include = "include";
-      for (int x = 0; x < include.length(); x++) {
-        if (line.at(x) == include.at(x)) {
-        } else {
-          break;
-        }
-        if (x == include.length() - 1) {
-          ignore = true;
-        }
-      }
+      // uses # to note a copiler options
+      std::string Flag = "";
+      if (line.at(0) == '#') {
+        // ignore = true;
+        int x = 1;
 
-      // checks to add line to string
-      if (ignore == false) {
-        text.append(line);
+        std::string Command = "";
+        std::string store = "";
+        bool running = true;
+        while (running == true) {
+          bool isSpace = false;
+          store = "";
+          while (isSpace == false) {
+            if (line.at(x) == ' ' || line.at(x) == '"' ||
+                x == line.length() - 1) {
+              isSpace = true;
+              if (Command == "" && Flag == "") {
+                Command = store;
+              } else if (Flag == "") {
+                Flag = store;
+              } else {
+                return "1"; // error value too many complier flag input
+              }
+            } else {
+              if (line.at(x) != ' ' && line.at(x) != '"') {
+                store += line.at(x);
+              }
+            }
+            x++;
+            if (x == line.length() - 1) {
+              running = false;
+            }
+          }
+        }
+        std::cout << Flag;
+        if (Command == "include") {
+          text = fileInput(text, Flag);
+        } else {
+          std::cout << "Compiler flag not recognise";
+        }
+
       } else {
-        std::cout << "placeholder";
-        // text = fileInput(text, file);
+        text.append(line);
       }
     }
     src.close();
   } else {
     std::cout << "Unable to open file";
+    return "2"; // error cannot open the file
   }
   src.close();
   return text;
