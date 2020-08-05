f92de56782596d7d26c6298b13660299def5e08e
Samuel Knapp <samuel.j.knapp@btinternet.com>
2020-08-05 17:46:53 +0100

    added a new folder to store assembly constructor functions

---
+++ src/assembly/ass.cpp
@@ -0,0 +1,9 @@
+//
+// Created by knapp on 05/08/2020.
+//
+
+namespace test {
+int test() { return 1; }
+} // namespace test
+
+#include "ass.h"
---
+++ src/assembly/ass.h
@@ -0,0 +1,12 @@
+//
+// Created by knapp on 05/08/2020.
+//
+
+#ifndef COMPILER_ASS_H
+#define COMPILER_ASS_H
+
+namespace test {
+int test();
+}
+
+#endif // COMPILER_ASS_H
